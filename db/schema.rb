6cb8e106f8dbf053034747e3e8a3c7b22d011502
Shawn Kearney <shawnpk@ineptsoftware.com>
2016-06-26 09:15:45 -0700

    Added functionality for todos to be marked complete

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160625230158) do
+ActiveRecord::Schema.define(version: 20160626160926) do
 
   create_table "todos", force: :cascade do |t|
     t.string   "title"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
     t.string   "email"
+    t.datetime "completed_at"
   end
 
 end
